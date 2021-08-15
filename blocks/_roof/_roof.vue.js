7f9f9c55cf25c7cab9f32853e073dae19dd9c64b
Pavel Fomin <webdev.fominpa@gmail.com>
2021-08-16 01:34:09 +0400

    decapsulation _header page element and _roof & _wrapper blocks

---
+++ blocks/_roof/_roof.vue.js
@@ -0,0 +1,12 @@
+export default {
+	template:
+	`<div class="roof">
+		<a href="index.html" class="logo">
+			<img src="images/logo.png" alt="logo">
+		</a>
+		<a href="#" class="burger">
+			<div class="burger__hl"></div>
+			<div class="burger__hl"></div>
+		</a>
+	</div>`
+}
---
+++ page-elements/_header/_header.vue.js
@@ -0,0 +1,16 @@
+import roof from '../../blocks/_roof/_roof.vue.js'
+
+export default {
+	components: {
+		roof,
+	}
+
+	template:
+	`<header class="header">
+		<div class="header__inner wrapper">
+			<roof></roof>
+			
+			<h1 class="header__title">we build innovative companies</h1>
+		</div>
+	</header>`
+}
