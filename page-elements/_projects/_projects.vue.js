62eb15fc68a8e740b131753e88f4b3a0b76efae1
Pavel Fomin <webdev.fominpa@gmail.com>
2021-08-16 01:58:04 +0400

    decapsulation _projects page element

---
+++ page-elements/_projects/_projects.vue.js
@@ -0,0 +1,27 @@
+import sectionTitle from '../../blocks/_section-title/_section-title.vue.js';
+
+export default {
+	components: {
+		sectionTitle,
+	},
+
+	template:
+	`<div class="projects">
+		<sectionTitle
+			class = 'projects__title'
+			number = '02'
+			title = 'projects'
+		></sectionTitle>
+
+		<div class="projects__inner wrapper">
+			<div class="projects__content">
+				<img class="projects__item" src="images/Strategy.png" alt="Strategy">
+				<img class="projects__item" src="images/Headphones.png" alt="Headphones">
+				<img class="projects__item" src="images/Badminton.png" alt="Badminton">
+				<img class="projects__item" src="images/and.png" alt="and">
+				<img class="projects__item" src="images/Sun Stone.png" alt="Sun Stone">
+				<img class="projects__item" src="images/sixth planet.png" alt="sixth planet">
+			</div>
+		</div>
+	</div>`
+}
